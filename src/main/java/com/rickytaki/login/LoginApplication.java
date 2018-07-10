e29c24f59e631b7312766090c19f399463ab4f3d
rickytaki <me@rickytaki.com>
2018-07-10 10:37:55 -0300

    Add model user

--- src/main/java/com/rickytaki/login/LoginApplication.java
+++ src/main/java/com/rickytaki/login/LoginApplication.java
@@ -2,8 +2,10 @@ package com.rickytaki.login;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class LoginApplication {
 
 	public static void main(String[] args) {
